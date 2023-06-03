9375f0eb0881f8a46eac0eb225ef7c02d738f9ab
Polina Semenova <polina-semenova-10@mail.ru>
2023-06-03 09:30:15 +0300

    Tasks completed

---
+++ Task_41/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] fillArrayWithInput(int size){
+    int[] array = new int[size];
+    for(int index = 0; index < array.Length; index++){
+        Console.Write($"Введите {index + 1}-й элемент массива: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        array[index] = num;
+    }
+    return array;
+}
+
+int[] result = fillArrayWithInput(size);
+Console.WriteLine(string.Join(", ", result));
+
+int findNumsMoreThanZero(int[] array){
+    int count = 0;
+    foreach(int num in array){
+        if (num > 0){
+            count += 1;
+        }
+    }
+    return count;
+}
+
+int amount = findNumsMoreThanZero(result);
+
+Console.WriteLine($"В целочисленном массиве [{string.Join(", ", result)}] {amount} чисел(ла) больше нуля");
---
+++ Task_43/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите значение b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double X_coord = (-b2 + b1)/(-k1 + k2);
+double Y_coord = k2 * X_coord + b2;
+
+Console.WriteLine($"Две прямые пересекутся в точке с координатами: X => {X_coord}, Y => {Y_coord}");
